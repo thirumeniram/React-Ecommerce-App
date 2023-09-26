d677254606594e1af064ad97570e8def037f1610
THIRUMENIRAM <66516937+thirumeniram@users.noreply.github.com>
2023-09-26 11:07:41 +0530

    Update Cart.js

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -1,33 +1,50 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { CartItems, DeleteCart } from "../../Redux/actions";
 
-// The 'Cart' component takes an 'item' prop which represents the product added to the cart.
+import { useDispatch } from "react-redux";
+import {
+  CartItems,
+  DeleteCart,
+  decrementQty,
+  addCart,
+} from "../../Redux/actions";
+
 export default function Cart({ item }) {
   const dispatch = useDispatch();
+  const dispatchCart = useDispatch();
+  const dispatchTotal = useDispatch();
 
-  // Function to handle the cancel (delete) action of a product from the cart
   function Delete(item) {
-    dispatch(DeleteCart(item)); // Dispatch the 'DeleteCart' action to remove the item from the cart in Redux store
-    dispatch(CartItems()); // Dispatch the 'CartItems' action to update the cart items after deletion
+    dispatch(DeleteCart(item));
+    dispatch(CartItems());
+  }
+
+  function handleCart(item) {
+    dispatchCart(addCart(item)); // Dispatch the 'addCart' action to add the item to the cart in the Redux store
+    dispatchTotal(CartItems()); // Dispatch the 'CartItems' action to update the cart items after adding the product
+    // Update the state to indicate that the item is added to the cart
+
+    window.alert("Item added to cart successfully");
+  }
+
+  function decrementItemQty(item) {
+    if (item.qty === 1) {
+      Delete(item);
+    } else {
+      dispatchCart(decrementQty(item)); // Dispatch action to decrement item quantity
+      dispatchTotal(CartItems());
+      window.alert("Item removed from cart successfully");
+    }
   }
 
   return (
     <>
-      {/* Container for the cart item */}
       <div className="d-flex container-sm p-1 bg-white gap-3" id="cart-item">
-        {/* Left part - Display product thumbnail */}
         <img src={item.thumbnail} alt="error" id="card-image" />
-
-        {/* Right part - Display product details */}
         <div
           className="d-flex flex-column gap-2 justify-content-start"
-          style={{ width: "50%", padding: "15px",overflowX:"hidden" }}
+          style={{ width: "50%", padding: "15px" }}
         >
-          {/* Product title */}
           <h2>{item.title}</h2>
-
-          {/* Product price */}
           <span className="text-success">
             <span className="text-danger" style={{ fontSize: "25px" }}>
               Price:
@@ -35,7 +52,32 @@ export default function Cart({ item }) {
             <span style={{ fontSize: "20px" }}>Rs{item.price}</span>
           </span>
 
-          {/* Cancel button to remove the product from the cart */}
+          <div className="align-self-end mt-5">
+            <button
+              type="button"
+              className="btn btn-outline-secondary"
+              onClick={() => decrementItemQty(item)}
+              id="decrement-button"
+            >
+              -
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-secondary"
+              id="qty-button"
+            >
+              {item.qty}
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-secondary"
+              onClick={() => handleCart(item)}
+              id="increment-button"
+            >
+              +
+            </button>
+          </div>
+
           <div className="align-self-end mt-5">
             <button
               type="button"
